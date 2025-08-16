bccd6465fb92ea569b8f836878e20dfd3e33e1cf
Lovish Goyal <goyallovish1727@example.com>
2025-08-17 01:06:57 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sems/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sems
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sems/features/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SEMS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
