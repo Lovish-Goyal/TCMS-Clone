5283b3c23eb274df75c11b2ba660bd2a28fa59c2
Lovish Goyal <goyallovish1727@example.com>
2025-08-20 11:38:58 +0530

    feat: add custom methods and firesbase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sems/features/home/home_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:sems/sems_app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Hive.initFlutter();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SEMS',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: HomeScreen(),
-    );
-  }
+  // await Hive.openBox('authBox');
+
+  runApp(const MainApp());
 }
